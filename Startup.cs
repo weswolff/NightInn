ea3a9bb07b496993eaed5b06170c287bd5cd08e0
weswolff <weswolff@gmail.com>
2020-12-14 20:16:42 -0500

    rebuilt project

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NightInnV2.Startup))]
+namespace NightInnV2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
