3d65b16fad9399d45e58187181da3077e32cd861
weswolff <weswolff@gmail.com>
2021-01-25 20:45:03 -0500

    Deleting Bootstrap themes

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace NightInnV2
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/fontawesome.css",
                       "~/Content/site.css"));
         }
     }
